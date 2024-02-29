3310b2122d8d073a532d608bbcf20a4f8578ac55
Moonqy <57512103+Moonqy@users.noreply.github.com>
2024-02-29 08:53:43 +0800

    Update MGMT-predict-train.py

--- MGMT-predict-train.py
+++ MGMT-predict-train.py
@@ -20,6 +20,8 @@ from monai.transforms import (
     RandRotate90d,
     CropForegroundd,
     ScaleIntensityd,
+    Activations, 
+    AsDiscrete,
 )
 from monai.utils import set_determinism
 import torch
@@ -29,12 +31,15 @@ import argparse
 from torch.optim import lr_scheduler
 from tqdm import tqdm
 from monai.metrics import ROCAUCMetric
+from sklearn.metrics import f1_score, recall_score
+
+sigmoid = torch.nn.Sigmoid()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--type", default="ADC", type=str)
+parser.add_argument("--type", default="T1", type=str)
 parser.add_argument("--model_name", default="DN121", type=str)
 parser.add_argument("--loss", default=1e-4, type=float)
-parser.add_argument("--max_epochs",default=50,type=int)
+parser.add_argument("--max_epochs",default=80,type=int)
 parser.add_argument("--pretrained",default=0,type=int)
 parser.add_argument("--segmented",default=0,type=int)
 parser.add_argument("--idh",default=0,type=int)
@@ -43,7 +48,6 @@ args = parser.parse_args()
 
 pin_memory = torch.cuda.is_available()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 mn=args.model_name
 pr=args.pretrained
 se=args.segmented
@@ -51,14 +55,12 @@ idh=args.idh
 ls=args.loss
 ty=args.type
 
-auc_metric = ROCAUCMetric()
-
 if args.segmented==1:
-    PATH_DATASET = 'your/segImgs'
-    batchnum=8
+    PATH_DATASET = '../../../../../brain-images/segImgs'
+    batchnum=4
     num_workers=8
 else:
-    PATH_DATASET = 'your/UNsegImgs'
+    PATH_DATASET = '../../../../../brain-images/UCSF-PDGM-v3'
     batchnum=2
     num_workers=4
 
@@ -88,17 +90,19 @@ elif args.type == 'SWI':
 elif args.type == 'DWI':
     imgtype='_DWI_bias.nii' 
 
-
+auc_metric = ROCAUCMetric()
 if args.idh==0:
-    df_train = pd.read_csv(('your-train.csv'),usecols=['ID', 'MGMT','fold'])
-    #class_weights = [410/113,410/297]
+    df_train = pd.read_csv(('../../../../../brain-images/UCSF-PDGM-v3/UCSF-MGMTF-train.csv'),usecols=['ID', 'MGMT','fold'])
+    class_weights = [410/113,410/297]
+    class_weights = torch.tensor(class_weights).to(device)
     weight_decay=0.01
 else:
-    df_train = pd.read_csv(('your-train.csv'),usecols=['ID', 'MGMT','fold']) 
-    #class_weights=[369/109,369/260]
+    df_train = pd.read_csv(('../../../../../brain-images/UCSF-PDGM-v3/UCSF-MGMT0F-train.csv'),usecols=['ID', 'MGMT','fold']) 
+    class_weights=[369/109,369/260]
+    class_weights = torch.tensor(class_weights).to(device)
     weight_decay=0.01
 images = []
-#class_weights = torch.tensor(class_weights).to(device)
+
 
 def readIMGs(imgPath,csvFile,imgtype,N):#读取图像的地址
     imglist=[]
@@ -128,6 +132,26 @@ def readIMGs(imgPath,csvFile,imgtype,N):#读取图像的地址
     labellist=np.array(labellist, dtype=np.int64)
     imglist=[{"img": img, "label": label} for img, label in zip(imglist, labellist)]
     return imglist
+
+def readIMGsTest(imgPath,csvFile,imgtype,foldN):#读取图像的地址
+    testfiles=[]
+    imglist=[]
+    labellist = []
+    for id, mgmt,fold in zip(csvFile['ID'],csvFile['MGMT'],csvFile['fold']):
+        if int(fold ) == foldN:
+            id=id[0:10]+'0'+id[10:]
+            #mgmt=torch.nn.functional.one_hot(torch.as_tensor(mgmt), num_classes=2).float()  
+            labellist.append(mgmt)   
+            if args.segmented==1:
+                imglist.append(os.path.join(imgPath,id+imgtype))
+            else :
+                if id=='0016':
+                    continue
+                imglist.append({os.path.join(imgPath,id+'_nifti',id+imgtype+'.gz')}) 
+    labellist=np.array(labellist, dtype=np.int64)
+    testfiles=[{"img": img, "label": label} for img, label in zip(imglist, labellist)]
+            
+    return testfiles
 
 def transformers():
     if args.segmented==0:
@@ -174,12 +198,17 @@ if __name__ == "__main__":
     print(f"--{args.model_name}--{args.type}-SET{pr}{se}{idh}--fold{args.fold}--BN {batchnum}--PN {pin_memory}-MP {args.max_epochs}-")
 
     trainlist=[]
+    testlist=[]
     trainlist=readIMGs(PATH_DATASET,df_train,imgtype,args.fold)
     
     train_transforms,val_transforms=transformers()
     
     train_ds=CacheDataset(data=trainlist,transform=train_transforms)
     train_loader=DataLoader(train_ds,batch_size=batchnum, shuffle=True, num_workers=num_workers,pin_memory=pin_memory)
+
+    testlist=readIMGsTest(PATH_DATASET,df_train,imgtype,args.fold)
+    test_ds=CacheDataset(data=testlist,transform=val_transforms)
+    test_loader=DataLoader(test_ds,batch_size=1, shuffle=False, num_workers=4,pin_memory=pin_memory)
 
     if args.pretrained==1:
         if args.model_name=='DN121':
@@ -225,9 +254,9 @@ if __name__ == "__main__":
             raise NotImplementedError
     model.to(device)
     
-   # loss_function = torch.nn.CrossEntropyLoss(weight=class_weights)
+    loss_function = torch.nn.CrossEntropyLoss(weight=class_weights)
     #loss_function
-    loss_function = torch.nn.CrossEntropyLoss()
+    #loss_function = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), args.loss,weight_decay=weight_decay)
     max_epochs=args.max_epochs
     val_interval = 2
@@ -236,11 +265,13 @@ if __name__ == "__main__":
     best_AUC_epoch = -1
     earlyend=6
     epoch_loss_values = []
-    #post_pred = Compose([Activations(softmax=True)])
-    #post_label = Compose([AsDiscrete(to_onehot=2)])
+    bestauc=-1
     minloss=100
     losscount=0
     for epoch in range(max_epochs):
+        post_pred = Compose([Activations(softmax=True)])
+        post_label = Compose([AsDiscrete(to_onehot=2)])
+
         epoch_loss = 0
         # 设置模型的训练模式
         model.train()
@@ -258,18 +289,45 @@ if __name__ == "__main__":
             optimizer.step()
             epoch_loss += loss.item()
             #print(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}")
+        with torch.no_grad():
+            y_pred = torch.tensor([], dtype=torch.float32, device=device)
+            y = torch.tensor([], dtype=torch.long, device=device)
+            epoch_iterator_train = tqdm(test_loader)
+            for step,test_data in enumerate(epoch_iterator_train):
+                test_image, label = test_data['img'].to(device), test_data['label'].to(device)
+                test_image = test_data['img'].to(device)
+                y_pred = torch.cat([y_pred, model(test_image)], dim=0)
+                y = torch.cat([y, label], dim=0)
 
+        acc_value = torch.eq(y_pred.argmax(dim=1), y)
+        acc_metric = acc_value.sum().item() / len(acc_value)
+
+        y_onehot = [post_label(i) for i in decollate_batch(y, detach=False)]
+        y_pred_act = [post_pred(i) for i in decollate_batch(y_pred)]
+
+        f1 = f1_score(y.cpu(), y_pred.cpu().argmax(dim=1), average='binary')
+        recall = recall_score(y.cpu(), y_pred.cpu().argmax(dim=1), average='binary')
+
+        auc_metric(y_pred_act, y_onehot)
+        auc_result = auc_metric.aggregate()
         epoch_loss /= step
         epoch_loss_values.append(epoch_loss)
-        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+        
+     
+        del y_pred_act, y_onehot,y,y_pred
+        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f} AUC: {auc_result:.4f} acc {acc_metric:.4f}  f1 {f1:.4f} recall {recall:.4f}")
+        if auc_result>bestauc-0.001:
+            losscount=0
+            bestauc=auc_result
+            torch.save(
+                model.state_dict(),
+                f"tempModel/{args.fold}.pth",
+                )
+            print(f"saved best model,best AUC:{auc_result:.4f}  acc {acc_metric:.4f}  f1 {f1:.4f} recall {recall:.4f}")
+        
         if epoch_loss<minloss-0.001:
             minloss=epoch_loss
             losscount=0
-        else:
-            losscount+=1
-        if losscount==earlyend:
+        if losscount>=earlyend:
+            print('early end!')
             break
-    torch.save(model.state_dict(),
-                    f"tempModel/{args.fold}.pth",
-                    )
-    torch.cuda.empty_cache()
